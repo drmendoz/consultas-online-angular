bdf907ebd78e64e495edb7eaf236e2da2361ad8e
David Roberto Mendoza Loor <drmendoz@espol.edu.ec>
2020-08-16 13:36:29 -0500

    Inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { TopBarComponent } from '../components/top-bar/top-bar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TopBarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
